110ebfabed583c362d709fc62ee1649a3d967ebe
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2023-08-02 02:16:57 +0900

    react + websocket works

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,7 +1,51 @@
-import React from "react";
+import { Box, Button, TextField, Typography } from "@mui/material";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
-  return <div>app</div>;
+  const [response, setResponse] = useState<string[]>([]);
+  const [message, setMessage] = useState("");
+  const wsRef = useRef<WebSocket>();
+
+  useEffect(() => {
+    const socket = new WebSocket("ws://localhost:8000/ws");
+    wsRef.current = socket;
+    socket.addEventListener("message", (e) => {
+      setResponse((r) => [...r, e.data]);
+    });
+    return () => socket.close();
+  }, []);
+  return (
+    <Box sx={{ m: 2 }}>
+      <Typography variant="h2">WebSocket Chat</Typography>
+      <TextField
+        label="text"
+        value={message}
+        onChange={(e) => {
+          e.preventDefault();
+          setMessage(e.target.value);
+        }}
+        size="small"
+      />
+      <Button
+        onClick={(e) => {
+          e.preventDefault();
+          if (!wsRef.current) return;
+          wsRef.current.send(message);
+        }}
+      >
+        send
+      </Button>
+      <Box>
+        {response.map((r, i) => {
+          return (
+            <Box sx={{ m: 1 }} key={i}>
+              {r}
+            </Box>
+          );
+        })}
+      </Box>
+    </Box>
+  );
 }
 
 export default App;
